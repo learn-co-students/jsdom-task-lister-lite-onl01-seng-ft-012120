7537987a2bcee49d2493d83fadcb587f1cbeef2d
Patrick Grady <patickrgrady81@gmail.com>
2020-04-17 10:51:33 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit", (e) => { 
+    e.preventDefault();
+    // get the value in the input
+    text = document.getElementById("new-task-description").value;
+    // add li with the value that was in the input
+    tasks = document.getElementById("tasks");
+    newLi = document.createElement("li");
+    newLi.innerHTML = text;
+    tasks.appendChild(newLi);
+  });
 });
