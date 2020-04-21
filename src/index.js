1317eb0e945e418ff887be3dc6ed9423b0897981
dwinniford <dwinniford@gmail.com>
2020-04-21 14:13:30 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submit = document.querySelector(".submit");
+  const newTaskDescription = document.querySelector("#new-task-description");
+  const list = document.querySelector("#tasks");
+  submit.addEventListener("click", function(event) {
+    const listItem = document.createElement("LI");
+    listItem.innerHTML = newTaskDescription.value;
+    list.appendChild(listItem);
+    event.preventDefault();
+  });
 });
