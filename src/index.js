ae1b13f58a62802bbf973ec57c1932960069f54d
andrew041994 <andrew041994@gmail.com>
2020-04-27 01:53:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let btn = document.getElementById("btn") 
+  let list = document.getElementById("list")
+
+function updateList(e) {
+  e.preventDefault();
+  let bar = document.getElementById("new-task-description").value
+  let node = document.createElement("li");
+  node.innerText = bar
+
+  list.appendChild(node)
+}
+btn.onclick = updateList
 });
